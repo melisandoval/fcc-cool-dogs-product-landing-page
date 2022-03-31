76855089fccb7a5b791b910ba5c490c998eeb798
melisand <melisandoval@live.com.ar>
2022-03-31 20:47:28 +0200

    Changed: header, local links, footer text.

--- product.js
+++ product.js
@@ -1,20 +1,20 @@
 // Hide Header
 // Code from https://www.w3schools.com/howto/howto_js_shrink_header_scroll.asp
 
-let prevScrollpos = window.pageYOffset;
+// let prevScrollpos = window.pageYOffset;
 
-window.onscroll = () => {
-  const currentScrollPos = window.pageYOffset;
+// window.onscroll = () => {
+//   const currentScrollPos = window.pageYOffset;
 
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-    document.getElementById("main").style.paddingTop = "200px";
-  } else {
-    document.getElementById("header").style.top = "-151px";
-    document.getElementById("main").style.paddingTop = "100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("header").style.top = "0";
+//     document.getElementById("main").style.paddingTop = "200px";
+//   } else {
+//     document.getElementById("header").style.top = "-151px";
+//     document.getElementById("main").style.paddingTop = "100px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
 
 // For the carrousel:
 // Code from: https://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
